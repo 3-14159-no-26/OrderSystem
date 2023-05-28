d858cc23af74b6b6ebd91bfa16be8e45a9818b76
sao-coding <jeter.nice@gmail.com>
2023-05-28 23:12:08 +0800

    📱 新增購物車響應式設計 &💄 新增購物車樣式

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -6,72 +6,113 @@ const Cart = () => {
     return (
         <>
             <Container>
-                <div className='w-full'>
-                    <div>Cart</div>
-                    {menuList.map((item) => (
-                        <div
-                            key={item.id}
-                            className='flex flex-row justify-between border-b-2 border-gray-200'
-                        >
-                            <div className='p-2'>
-                                <div className='flex'>
-                                    <img
-                                        className='w-20 h-20 object-cover rounded-lg'
-                                        src={"https://picsum.photos/200/100?random=" + item.id}
-                                        alt=''
-                                    />
-                                    <div className=''>
-                                        <div className='flex'>
-                                            <div className='p-2'>品名</div>
-                                            <div className='p-2'>{item.name}</div>
-                                        </div>
-                                        <div className='flex'>
-                                            <div className='p-2'>單價</div>
-                                            <div className='p-2'>{item.price}</div>
-                                        </div>
+                <div className='w-full rounded-2xl border-2 border-black bg-[#efa987]'>
+                    <div className='relative z-10 m-[-2px] h-40 border-collapse rounded-2xl border-2 border-black px-4 pb-4 shadow-xl'>
+                        <div className='flex h-full w-full items-center justify-center rounded-b-xl border-x-2 border-b-2 border-dashed border-[#ae7252]'>
+                            <div className=''>
+                                <div className='m-2 flex items-center justify-center'>
+                                    <div className='text-3xl'>WH Eats 帳單本</div>
+                                </div>
+                                <div className='flex items-center justify-center'>
+                                    <div className='rounded-full border-2 border-dashed border-black bg-[#fbc3a5] px-4 py-1'>
+                                        記得要結帳喔~
                                     </div>
                                 </div>
-                            </div>
-                            <div className='p-2'>
-                                <div className='grid grid-cols-2'>
-                                    <div className='p-2'>數量</div>
-                                    <div className='p-2  justify-self-end'>{item.count}</div>
-                                    <div className='p-2'>小計</div>
-                                    <div className='p-2  justify-self-end'>
-                                        {item.price * item.count}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                    <div className='border-b-2 border-gray-200'>
-                        <div className='flex justify-between'>
-                            <div className='p-2'>總計</div>
-                            <div className='p-2'>
-                                {menuList.reduce((total, item) => {
-                                    console.log(
-                                        "目前加總",
-                                        total,
-                                        "目前項目 =>",
-                                        "單價",
-                                        item.price,
-                                        "數量",
-                                        item.count
-                                    )
-
-                                    return total + item.price * item.count
-                                }, 0)}
                             </div>
                         </div>
                     </div>
-                    <div className='flex mt-4'>
-                        <button
-                            className='bg-amber-400 p-2 rounded-lg w-full m-1'
-                            onClick={() => resetToCart()}
-                        >
-                            清空購物車
-                        </button>
-                        <button className='bg-amber-400 p-2 rounded-lg w-full m-1'>結帳</button>
+                    <div className='px-4 pb-4'>
+                        <div className='rounded-b-xl border-x-2 border-b-2 border-dashed border-[#ae7252] px-4 pb-4'>
+                            <div className='relative'>
+                                <div className='details-list '>
+                                    <div className='w-full bg-white p-4'>
+                                        <div className='flex items-center justify-center p-4'>
+                                            <div className='text-3xl'>單號:087</div>
+                                        </div>
+                                        <div className='flex items-center justify-center px-4 pb-4'>
+                                            <div className='text-2xl'>時間 2023/09/87</div>
+                                        </div>
+                                        {menuList.map((item) => (
+                                            <div
+                                                key={item.id}
+                                                className='flex flex-row justify-between border-b-2 border-gray-200 max-md:block'
+                                            >
+                                                <div className='p-2'>
+                                                    <div className='flex max-md:justify-between'>
+                                                        <div className='max-md:order-2'>
+                                                            <img
+                                                                className='h-20 w-20 rounded-lg object-cover'
+                                                                src={
+                                                                    "https://picsum.photos/200/100?random=" +
+                                                                    item.id
+                                                                }
+                                                                alt=''
+                                                            />
+                                                        </div>
+                                                        <div className='max-md:order-1'>
+                                                            <div className='flex'>
+                                                                <div className='p-2'>品名</div>
+                                                                <div className='p-2'>
+                                                                    {item.name}
+                                                                </div>
+                                                            </div>
+                                                            <div className='flex'>
+                                                                <div className='p-2'>單價</div>
+                                                                <div className='p-2'>
+                                                                    {item.price}
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='p-2 max-md:p-0 max-md:px-2 max-md:pb-2'>
+                                                    <div className='grid grid-cols-2'>
+                                                        <div className='p-2'>數量</div>
+                                                        <div className='justify-self-end  p-2'>
+                                                            {item.count}
+                                                        </div>
+                                                        <div className='p-2'>小計</div>
+                                                        <div className='justify-self-end  p-2'>
+                                                            {item.price * item.count}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                        <div className='border-b-2 border-gray-200'>
+                                            <div className='flex justify-between text-2xl'>
+                                                <div className='p-2'>總計</div>
+                                                <div className='p-2'>
+                                                    {menuList.reduce((total, item) => {
+                                                        console.log(
+                                                            "目前加總",
+                                                            total,
+                                                            "目前項目 =>",
+                                                            "單價",
+                                                            item.price,
+                                                            "數量",
+                                                            item.count
+                                                        )
+                                                        return total + item.price * item.count
+                                                    }, 0)}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className='mt-4 flex'>
+                                            <button
+                                                className='m-1 w-full rounded-lg bg-amber-400 p-2'
+                                                onClick={() => resetToCart()}
+                                            >
+                                                清空購物車
+                                            </button>
+                                            <button className='m-1 w-full rounded-lg bg-amber-400 p-2'>
+                                                結帳
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Container>
