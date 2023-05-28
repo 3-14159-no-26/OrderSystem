fc64371029f7b78c806d2de93bd7c1020d8b35d5
sao-coding <jeter.nice@gmail.com>
2023-05-28 23:09:38 +0800

    🧑‍💻修改 開發模式URL

--- src/url.tsx
+++ src/url.tsx
@@ -1,1 +1,7 @@
-export default "https://json-server-vercel-ts.vercel.app/api"
+// 如果是開發模式
+const url =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:3001"
+        : "https://json-server-vercel-ts.vercel.app/api"
+
+export default url
