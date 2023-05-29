dc254aed793319bcfab0fa92b5e43ed5bc9c0499
sao-coding <jeter.nice@gmail.com>
2023-05-30 01:41:27 +0800

    💄新增 過度動畫圖片樣式

--- src/pages/home/components/Loading.tsx
+++ src/pages/home/components/Loading.tsx
@@ -1,3 +1,5 @@
+import { IconPhoto } from "@tabler/icons-react"
+
 const Loding = () => {
     const item = () => {
         const item = []
@@ -8,7 +10,12 @@ const Loding = () => {
                     key={i}
                 >
                     <div className='m-4 rounded-xl bg-slate-900 max-md:w-full'>
-                        <div className='h-[200px] w-[200px] rounded-lg bg-slate-200 max-md:w-full'></div>
+                        <div className='h-[200px] w-[200px] rounded-lg bg-slate-200 max-md:w-full'>
+                            <div className='flex h-full items-center justify-center'>
+                                <IconPhoto size={48} />
+                                <div className='text-2xl'>圖片</div>
+                            </div>
+                        </div>
                         <div className='flex flex-col justify-center p-4'>
                             <div className='h-4 w-10 rounded-full bg-slate-200'></div>
                             <div className='flex justify-end pt-4'>
--- src/pages/menu/components/Loading.tsx
+++ src/pages/menu/components/Loading.tsx
@@ -1,8 +1,15 @@
+import { IconPhoto } from "@tabler/icons-react"
+
 const Loding = () => {
     return (
         <div className='flex animate-pulse flex-wrap ring-slate-900/5'>
             <div className='m-4 rounded-xl bg-slate-900 p-2 '>
-                <div className='h-[100px] w-[200px] rounded-lg bg-slate-200'></div>
+                <div className='h-[100px] w-[200px] rounded-lg bg-slate-200'>
+                    <div className='flex h-full items-center justify-center'>
+                        <IconPhoto size={48} />
+                        <div className='text-2xl'>圖片</div>
+                    </div>
+                </div>
                 <div className='flex flex-col justify-center p-4'>
                     <div className='h-4 w-10 rounded-full bg-slate-200'></div>
                     <div className='mt-2 h-4 w-20 rounded-full bg-slate-200'></div>
