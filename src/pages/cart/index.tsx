54acf6ae3a4000cb54824f1c038f798f7a42d963
sao-coding <jeter.nice@gmail.com>
2023-05-29 16:36:54 +0800

    ♻️ 重購 購物車代碼

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import { useMenuListContext } from "@/context/MenuList"
 import Container from "@/components/container"
-import MenuItem from "@/pages/cart/components/menuItem"
 
 const Cart = () => {
     const go = useNavigate()
@@ -38,7 +37,66 @@ const Cart = () => {
                                                 <div className='flex items-center justify-center px-4 pb-4'>
                                                     <div className='text-2xl'>時間 2023/09/87</div>
                                                 </div>
-                                                <MenuItem menuList={menuList} />
+                                                {menuList.map((item) => (
+                                                    <div
+                                                        key={item.id}
+                                                        className='cursor-pointer border-b-2 border-gray-200 p-1'
+                                                    >
+                                                        <div
+                                                            className='flex w-full items-center justify-between rounded-2xl hover:bg-gray-100 max-md:block'
+                                                            onClick={() => {
+                                                                go(`/menu/${item.id}`, {
+                                                                    state: { prevPath: "/cart" },
+                                                                })
+                                                            }}
+                                                        >
+                                                            <div className='p-2'>
+                                                                <div className='flex max-md:justify-between'>
+                                                                    <div className='max-md:order-2'>
+                                                                        <img
+                                                                            className='h-20 w-20 rounded-lg object-cover'
+                                                                            src={
+                                                                                "https://picsum.photos/200/100?random=" +
+                                                                                item.id
+                                                                            }
+                                                                            alt=''
+                                                                        />
+                                                                    </div>
+                                                                    <div className='max-md:order-1'>
+                                                                        <div className='flex'>
+                                                                            <div className='p-2'>
+                                                                                品名
+                                                                            </div>
+                                                                            <div className='p-2'>
+                                                                                {item.name}
+                                                                            </div>
+                                                                        </div>
+                                                                        <div className='flex'>
+                                                                            <div className='p-2'>
+                                                                                單價
+                                                                            </div>
+                                                                            <div className='p-2'>
+                                                                                {item.price}
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div className='p-2 max-md:p-0 max-md:px-2 max-md:pb-2'>
+                                                                <div className='grid grid-cols-2'>
+                                                                    <div className='p-2'>數量</div>
+                                                                    <div className='justify-self-end  p-2'>
+                                                                        {item.count}
+                                                                    </div>
+                                                                    <div className='p-2'>小計</div>
+                                                                    <div className='justify-self-end  p-2'>
+                                                                        {item.price * item.count}
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                ))}
                                                 <div className='border-b-2 border-gray-200'>
                                                     <div className='flex justify-between text-2xl'>
                                                         <div className='p-2'>總計</div>
