01b926668986de9f782ab4ef47bf6d82b7287545
sao-coding <jeter.nice@gmail.com>
2023-06-01 10:25:40 +0800

    ✨ 新增手機板導航欄 購物車圖標

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-// import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { useMenuListContext } from "@/context/MenuList"
 import {
     IconToolsKitchen2,
@@ -13,6 +13,7 @@ const Footer = () => {
     const [prevScrollPos, setPrevScrollPos] = useState(0)
     const [navbarHidden, setNavbarHidden] = useState(false)
     const { menuList } = useMenuListContext()
+    const go = useNavigate()
 
     useEffect(() => {
         const handleScroll = () => {
@@ -42,19 +43,33 @@ const Footer = () => {
                     { hidden: navbarHidden }
                 )}
             >
-                <div className='flex flex-col items-center justify-center'>
+                <div className='flex flex-col items-center justify-center' onClick={() => go("/")}>
                     <IconToolsKitchen2 />
                     <div className='mt-1'>訂餐</div>
                 </div>
-                <div className='flex flex-col items-center justify-center'>
+                <div
+                    className='relative flex flex-col items-center justify-center'
+                    onClick={() => go("/cart")}
+                >
+                    {menuList.length > 0 && (
+                        <div className='absolute -top-1 right-2 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 pt-[0.15rem] text-white'>
+                            <div className='text-center text-xs'>{menuList.length}</div>
+                        </div>
+                    )}
                     <IconShoppingCart />
                     <div className='mt-1'>購物車</div>
                 </div>
-                <div className='flex flex-col items-center justify-center'>
+                <div
+                    className='flex flex-col items-center justify-center'
+                    onClick={() => go("/details")}
+                >
                     <IconClipboardCheck />
                     <div className='mt-1'>訂單</div>
                 </div>
-                <div className='flex flex-col items-center justify-center'>
+                <div
+                    className='flex flex-col items-center justify-center'
+                    onClick={() => go("/user")}
+                >
                     <IconUser />
                     <div className='mt-1'>會員</div>
                 </div>
