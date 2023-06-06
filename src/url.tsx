5a84f7433f1e4432959bcf7199c2791ced9d71e9
sao-coding <jeter.nice@gmail.com>
2023-06-07 02:17:09 +0800

    API 先改回來

--- src/url.tsx
+++ src/url.tsx
@@ -1,5 +1,7 @@
 // 如果是開發模式
 const url =
-    process.env.NODE_ENV === "development" ? "http://localhost:3001" : "http://localhost:14589/api"
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:3001"
+        : "https://json-server-vercel-ts.vercel.app/api"
 
 export default url
