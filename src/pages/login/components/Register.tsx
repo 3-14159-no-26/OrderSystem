55b961142262fe7cf0f1eb4b8ac7cc69ae6cfbc2
sao-coding <jeter.nice@gmail.com>
2023-06-07 02:17:58 +0800

    ✨ 新增註冊功能

--- src/pages/login/components/Register.tsx
+++ src/pages/login/components/Register.tsx
@@ -2,20 +2,25 @@ import { useState, useEffect } from "react"
 import {
     IconPhone,
     IconMail,
+    IconUser,
     IconUserCircle,
     IconLock,
     IconExclamationCircle,
 } from "@tabler/icons-react"
+import { v4 as uuidv4 } from "uuid"
+import { ToastContainer, toast } from "react-toastify"
 import clsx from "clsx"
 
 const Register = ({ status }: { status: (status: string) => void }) => {
     const [phone, setPhone] = useState("")
     const [email, setEmail] = useState("")
+    const [name, setName] = useState("")
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [password2, setPassword2] = useState("")
     const [errorPhone, setErrorPhone] = useState(false)
     const [errorEmail, setErrorEmail] = useState(false)
+    const [errorName, setErrorName] = useState(false)
     const [errorUsername, setErrorUsername] = useState(false)
     const [errorPassword, setErrorPassword] = useState(false)
     const [errorPassword2, setErrorPassword2] = useState(false)
@@ -50,14 +55,27 @@ const Register = ({ status }: { status: (status: string) => void }) => {
     }, [email])
 
     useEffect(() => {
+        const isValiName = () => {
+            return name.length <= 5
+        }
+        if (!isValiName()) {
+            console.log("名字最多只能五個字")
+            setErrorName(true)
+        } else {
+            console.log("name is valid")
+            setErrorName(false)
+        }
+    }, [name])
+
+    useEffect(() => {
         const isValiUsername = () => {
             // 必須是6-20位字符且要包含一個英文其他的都是數字能用 - _
-            const re = /^(?=.*[a-zA-Z])[a-zA-Z\d_-]{6,20}$/
+            const re = /^(?=.*[a-zA-Z])[a-zA-Z\d_-]{4,20}$/
             return re.test(username)
         }
         if (!isValiUsername()) {
             console.log(
-                "請輸入 6 到 20 個字符的字串，必須包含至少一個英文字母。允許使用數字、連字符和下劃線作為特殊字符。"
+                "請輸入 4 到 20 個字符的字串，必須包含至少一個英文字母。允許使用數字、連字符和下劃線作為特殊字符。"
             )
             setErrorUsername(true)
         } else {
@@ -97,28 +115,44 @@ const Register = ({ status }: { status: (status: string) => void }) => {
     }, [password2])
 
     useEffect(() => {
-        if (errorPhone || errorEmail || errorUsername || errorPassword || errorPassword2) {
+        if (
+            errorPhone ||
+            errorEmail ||
+            errorName ||
+            errorUsername ||
+            errorPassword ||
+            errorPassword2
+        ) {
             setButtonDisabled(true)
         } else {
             setButtonDisabled(false)
         }
-    }, [errorPhone, errorEmail, errorUsername, errorPassword, errorPassword2])
+    }, [errorPhone, errorEmail, errorName, errorUsername, errorPassword, errorPassword2])
 
     useEffect(() => {
         setErrorPhone(false)
         setErrorEmail(false)
+        setErrorName(false)
         setErrorUsername(false)
         setErrorPassword(false)
         setErrorPassword2(false)
     }, [])
 
-    const register = () => {
-        if (errorPhone || errorEmail || errorUsername || errorPassword || errorPassword2) {
+    const register = async () => {
+        if (
+            errorPhone ||
+            errorEmail ||
+            errorName ||
+            errorUsername ||
+            errorPassword ||
+            errorPassword2
+        ) {
             return
         }
         if (
             phone === "" ||
             email === "" ||
+            name === "" ||
             username === "" ||
             password === "" ||
             password2 === ""
@@ -126,10 +160,54 @@ const Register = ({ status }: { status: (status: string) => void }) => {
             return
         }
         console.log("register")
+        try {
+            const res = await fetch(`${URL}/user/register`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    customerID: uuidv4(),
+                    username: username,
+                    userpwd: password,
+                    email: email,
+                    phone: phone,
+                    // 目前台灣時間格式化 yyyy-mm-dd
+                    Bdate: new Date().toLocaleString("zh-TW", {
+                        timeZone: "Asia/Taipei",
+                        year: "numeric",
+                        month: "2-digit",
+                        day: "2-digit",
+                    }),
+                }),
+            })
+            const data = await res.json()
+            console.log(data)
+            if (data.status === "success") {
+                toast.success("註冊成功")
+                status("login")
+            } else {
+                toast.error("註冊失敗")
+            }
+        } catch (error) {
+            toast.error("註冊失敗")
+        }
     }
 
     return (
         <>
+            <ToastContainer
+                position='top-right'
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme='light'
+            />
             <div className='m-2 flex flex-col items-center justify-center rounded-lg bg-white p-2 shadow'>
                 <div className='p-2 text-xl font-semibold'>歡迎使用！線上點餐系統</div>
                 <div className='p-2'>請選擇登入/註冊方式，註冊後即可使用</div>
@@ -206,6 +284,35 @@ const Register = ({ status }: { status: (status: string) => void }) => {
                 <div className='m-4'>
                     <div
                         className={clsx(
+                            "flex items-center rounded-full border bg-white p-1 text-xl max-md:mx-0",
+                            errorName
+                                ? "border-2 border-red-500"
+                                : "border-gray-400 hover:border-gray-600"
+                        )}
+                    >
+                        <IconUser className='px-2' size={40} />
+                        <input
+                            type='text'
+                            className='box-border w-full rounded-r-full p-1 focus-visible:outline-none'
+                            onChange={(e) => {
+                                setName(e.target.value)
+                            }}
+                            value={name}
+                            placeholder='姓名'
+                        />
+                        <IconExclamationCircle
+                            className={clsx("text-red-500", errorName ? "" : "hidden")}
+                            size={40}
+                            stroke={2}
+                        />
+                    </div>
+                    {errorName && (
+                        <div className='text-right text-sm text-red-500'>名字最多只能五個字</div>
+                    )}
+                </div>
+                <div className='m-4'>
+                    <div
+                        className={clsx(
                             "flex items-center rounded-full border bg-white p-1 text-xl",
                             errorUsername
                                 ? "border-2 border-red-500"
@@ -230,8 +337,8 @@ const Register = ({ status }: { status: (status: string) => void }) => {
                     </div>
                     {errorUsername && (
                         <div className='text-right text-sm text-red-500'>
-                            包含至少一個字母和一個數字，且只能由字母和數字組成，長度介於 6 到 20
-                            個字符之間。
+                            請輸入 4 到 20
+                            個字符的字串，必須包含至少一個英文字母。允許使用數字、連字符和下劃線作為特殊字符。
                         </div>
                     )}
                 </div>
