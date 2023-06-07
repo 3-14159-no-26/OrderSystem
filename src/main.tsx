5b670cfcb9c3e74059a91d37e80ce7c1061463c9
sao-coding <jeter.nice@gmail.com>
2023-06-08 01:16:15 +0800

    ✨ 新增 vercel 網站分析

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 // import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { inject } from "@vercel/analytics"
 import App from "@/App.tsx"
 import "@/tailwind.css"
 import "@/index.css"
-
+inject()
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     // <React.StrictMode>
     <BrowserRouter>
