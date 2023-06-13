d18fa60544c9b3cd7d7e479588206bc74c9a01cd
sao-coding <jeter.nice@gmail.com>
2023-06-13 15:08:40 +0800

    💄 修改 404 頁面樣式

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -3,7 +3,7 @@ import "@/assets/css/404.css"
 const NotFound: React.FC = () => {
     return (
         <>
-            <div className='NotFound absolute z-30 max-h-[85vh] w-screen'>
+            <div className='NotFound absolute z-0 max-h-[85vh] w-screen bg-slate-50 text-black dark:bg-[#0f0f0f] dark:text-white'>
                 <div className='error'>
                     <div className='wrap'>
                         <div className='404'>
